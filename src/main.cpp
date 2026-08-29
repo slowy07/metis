1432a247804b1ba32636d1a6c98657708b179560
slowy07 <slowy.arfy@proton.me>
2026-08-29 11:55:42 +0700

    chore: bump version (0.5.0)
    Signed-off-by: slowy07 <slowy.arfy@proton.me>

--- src/main.cpp
+++ src/main.cpp
@@ -155,7 +155,7 @@ int main(int argc, char** argv) {
   std::string config_path = preparse_config_path(args);
 
   ArgParser app("metis", "Fast C++20-powered pre-commit & CI generator");
-  app.set_version("0.4.0")
+  app.set_version("0.5.0")
       .add_option("-c", "--config", "Config file path", config_path)
       .add_subcommand("init", "Create default .metis.toml")
       .add_subcommand("install", "Generate & install .git/hooks/pre-commit")
